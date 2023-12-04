865a908ab08252085e27fecc876ffc83d4152ce7
Nabhanshu <119276180+Nabhanshu@users.noreply.github.com>
2023-12-04 12:36:51 +0530

    added app.js

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Card from './componenets/Card';
+
+
+function App() {
+  return (
+   
+    <div className='gridbox'>
+      < Card   name="Nabh" email="royalnabhanshu@gmail.com" image='https://via.placeholder.com/200' /> 
+       < Card   name="Sarthi " email="vashisth@gamil.com" image='https://via.placeholder.com/200'/> 
+       < Card    name="Arjun" email="sharma.@gamil.com" image='https://via.placeholder.com/200'/>
+       </div>
+
+  );
+
+  }
+export default App;
+
